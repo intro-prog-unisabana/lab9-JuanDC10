bbea78aad9940ced80da206706fb8418dca5b935
JuanDC10 <juancastpar@unisabana.edu.co>
2026-05-01 11:45:50 -0500

    4.1

--- car.py
+++ car.py
@@ -1,2 +1,17 @@
 # Write your class definition here!
-
+class Car: 
+    def __init__init__(self, car_id, brand, year, color, mileage=0.0): 
+        self.car_id = car_id
+        self.brand = brand 
+        self.year = year
+        self.color = color 
+        self.mileage = mileage
+    def change_color(self, new_color): 
+        self.color = new.color
+        return f"The car is now {self.color}."
+    def drive(self, miles): 
+        miles = 0.6214
+        self.mileage = self.mileage * miles
+    def __str__(self): 
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.mileage}"
+    
