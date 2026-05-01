36aeb7511447da8cd0c48f8bccea6806e095a654
JuanDC10 <juancastpar@unisabana.edu.co>
2026-05-01 14:53:43 -0500

    6.1

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,5 +1,7 @@
 from aircraft import Aircraft
 
+from aircraft import Aircraft
+
 def main():
     model = input("Enter aircraft model:\n")
     plane = Aircraft(model)
@@ -11,6 +13,11 @@ def main():
             break
 
         parts = command.split()
+
+        # Seguridad extra (evita crashes en tests)
+        if len(parts) != 2:
+            continue
+
         action = parts[0]
         feet = int(parts[1])
 
@@ -20,7 +27,6 @@ def main():
             plane.descent(feet)
 
     print(f"Final altitude: {plane.altitude} feet")
-
 
 if __name__ == "__main__":
     main()
